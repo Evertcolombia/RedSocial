782c0e49db683f1aa7fc38625703909e1cf5a075
evertcolombia <evertescalante@gmail.com>
2020-05-16 16:07:01 -0500

    first task till do first migration between persistence-api and db

---
+++ Domain/value.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Value
+    {
+        public int Id { get; set; } // get and set a random value
+        public string Name { get; set; }
+    }
+}
